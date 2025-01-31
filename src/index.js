9f6a3c1f6a94cf403d8e7928d3cf706a62fc0f03
vsiegrist <vickysiegrist@gmail.com>
2025-01-31 11:01:37 -0500

    Added image function

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
@@ -14,6 +15,9 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
   timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img
+                  class="weather-app-icon"
+                  src="${response.data.condition.icon_url}" />`;
 }
 
 function formatDate(date) {
