2a687e2a1d0cf92a7271d226959b8fabf790599c
vsiegrist <vickysiegrist@gmail.com>
2025-02-05 14:19:13 -0500

    Added Forecast JavaScript

--- src/index.js
+++ src/index.js
@@ -57,3 +57,29 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Louisville");
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+displayForecast();
